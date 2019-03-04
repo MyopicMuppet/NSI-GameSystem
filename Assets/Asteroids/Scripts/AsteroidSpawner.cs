8cc9b81ef926efb47e77d68b01eca710c94ac0c4
MyopicMuppet <47373110+MyopicMuppet@users.noreply.github.com>
2019-03-04 11:57:21 +1100

    Asteroid game update

---
+++ Assets/Asteroids/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asteroids
+{
+
+    public class AsteroidSpawner : MonoBehaviour
+    {
+
+        // Array of prefabs to spawn
+        public GameObject[] asteroidPrefabs;
+        // Rate of spawn (in seconds)
+        public float spawnRate = 1f;
+        // Distance to spawn each asteroid
+        public float spawnRadius = 5f;
+
+        void Spawn()
+        {
+            //Randomize a position
+            Vector2 rand = Random.insideUnitCircle * spawnRadius;
+            //Offset position from spawner location
+            Vector2 position = (Vector2)transform.position + rand;
+            // Generate a random index into prefab array
+            int randIndex = Random.Range(0, asteroidPrefabs.Length);
+            // Get random asteroid using index
+            GameObject randAsteroid = asteroidPrefabs[randIndex];
+            // Clone random asteroid
+            Instantiate(randAsteroid, position, Quaternion.identity);
+
+
+        }
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
---
+++ Assets/Asteroids/Scripts/Movement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Units to travel per second
+    public float speed = 20f;
+    // Amount of rotation per second
+
+    // Reference to attached Rigidbody2D
+    private Rigidbody2D rigid;
+
+    public float rotationSpeed = 360f;
+    // Use this for initialization
+    void Start()
+    {
+        // Grab reference to rigidbody2d component
+        // NOTE: It gets this from the GameObject this script is attached to
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if A key is pressed
+        if (Input.GetKey(KeyCode.A))
+        {
+            //Rotate left
+            transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+        }
+
+        // Check if D key is pressed
+        if (Input.GetKey(KeyCode.D))
+        {
+            // Rotate Right
+            transform.Rotate(Vector3.forward, -rotationSpeed * Time.deltaTime);
+        }
+
+        // Check if W is pressed
+        if (Input.GetKey(KeyCode.W))
+        {
+            // Move in facing direction
+            rigid.AddForce(transform.up * speed);
+        }
+
+        // Check if S key is pressed
+        if (Input.GetKey(KeyCode.S))
+        {
+            // Move in facing direction
+            rigid.AddForce(-transform.up * speed);
+        }
+
+    }
+}
